139fcdcb65714579f4cb890a69de6f3fcce92588
Mitesh Parida <107741@tfcorp.com>
2024-03-25 15:54:57 +0530

    my first commit ever

---
+++ src/demopackage/A.java
@@ -0,0 +1,11 @@
+package demopackage;
+
+public class A {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hello mitesh. This will be your first git commit or push");
+
+	}
+
+}
